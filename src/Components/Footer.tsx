c781cc47307e5f9515788d8bd8741ab675677424
siva0704 <vaibhavabg1234@gmail.com>
2024-09-17 21:07:47 +0530

    created nav and carousel

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,18 +1,43 @@
-export const Footer: React.FC<{}> = () => { 
-    
-    return <>
-    
-    <h1>
+// src/components/Footer.tsx
+import React from "react";
 
-Footer
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-4">
+      <div className="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center">
+        {/* Left Section */}
+        <div className="text-center sm:text-left">
+          <p className="text-sm">&copy; 2024 Brave SparKids. All rights reserved.</p>
+        </div>
 
-    </h1>
-    
-    
-    
-    </>
+        {/* Center Section */}
+        <div className="my-2 sm:my-0">
+          <a
+            href="#"
+            className="text-gray-400 hover:text-white mx-2"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="#"
+            className="text-gray-400 hover:text-white mx-2"
+          >
+            Terms of Service
+          </a>
+        </div>
 
+        {/* Right Section */}
+        <div className="text-center sm:text-right">
+          <p className="text-sm">
+            Follow us:
+            <a href="#" className="text-gray-400 hover:text-white mx-2">Facebook</a>
+            <a href="#" className="text-gray-400 hover:text-white mx-2">Twitter</a>
+            <a href="#" className="text-gray-400 hover:text-white mx-2">LinkedIn</a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-
-
-}
+export default Footer;
