07656b1fdb3345ec6b5d8c9506128f724518bf77
siva0704 <vaibhavabg1234@gmail.com>
2024-09-30 21:49:24 +0530

    k

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -1,10 +1,13 @@
 import { Cards } from "../Components/Cards"
+import { ContactUs } from "./ContactUs"
 
 export const AboutUs: React.FC<{}> = () => { 
     
     return <>
     
 <Cards/>
+
+<ContactUs/>
     
     
     </>
